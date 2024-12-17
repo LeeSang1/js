bb926c05e68590f2f11d129958dd5bd7eaf123eb
LeeSang1 <les1305@naver.com>
2024-12-17 23:10:44 +0900

    현재 파일 커밋

---
+++ ONEBITE_REACT/section05/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+// import Bulb from "./components/Bulb";
+// import Counter from "./components/Counter";
+import Register from "./components/Register";
+import HookExam from "./components/HookExam";
+
+function App() {
+  return (
+    <>
+      <Register></Register>
+      {/* <Bulb />
+      <Counter /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ ONEBITE_REACT/section06/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+
+function App() {
+  return (
+    <>
+      <Viewer></Viewer>
+    </>
+  );
+}
+
+export default App;
